58f0b69d06a8285e3fdc0780e0e2b87f059ce481
sungjoon92 <wsjtjdwns92@naver.com>
2025-01-23 18:44:06 +0900

    react-spring docker 프론트 페이지

--- dockerfile/backend/src/main/java/com/example/backend/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
